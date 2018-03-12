5eca22fa479318bb5769f0abf1e7f1fb26b7f08c
Dan MacDonald <allcoms@gmail.com>
2018-03-12 17:23:16 +0000

    Add Velon script

--- Velon.gd
+++ Velon.gd
@@ -6,7 +6,7 @@ signal scoreinc
 
 onready var mainnode = get_node("/root/Main")
 
-func _on_Ovni_area_entered(area):
+func _on_Velon_area_entered(area):
 	mainnode.connect("scoreinc", self, "score_increase")
 	alph -= .1
 	if alph > .1:
