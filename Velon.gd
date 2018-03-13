e1290c715bde535b0bad2ee694552c9d8221c12c
Dan MacDonald <allcoms@gmail.com>
2018-03-13 23:04:40 +0000

    Make Velons bomb continuously at random intervals

--- Velon.gd
+++ Velon.gd
@@ -3,21 +3,20 @@ extends Area2D
 export (PackedScene) var Bomb
 
 var alph = 1
-#var timer = null
+var timer = null
+var nmepos = get_position()
+var bombpos = Vector2()
+var bdelay = rand_range(1, 5)
 
 signal scoreinc
 
-onready var mainnode = get_node("/root/Main")
-
 func _ready():
-	bomb()
-#	var bdelay = rand_range(1, 5)
-#	timer = Timer.new()
-#	timer.set_one_shot(true)
-#	timer.set_wait_time(bdelay)
-#	timer.connect("timeout", self, "bomb")
-#	add_child(timer)
-#	timer.start()
+	timer = Timer.new()
+	timer.set_one_shot(true)
+	timer.set_wait_time(bdelay)
+	timer.connect("timeout", self, "bomb")
+	add_child(timer)
+	timer.start()
 
 func _process(delta):
 	if has_node("bomb"):
@@ -26,19 +25,19 @@ func _process(delta):
 		get_node("bomb").set_position(bombpos)
 		if bombpos.y > 480:
 			get_node("bomb").queue_free()
+			bdelay = rand_range(1, 5)
+			timer.set_wait_time(bdelay)
+			timer.start()
 
 func bomb():
     var bomb = Bomb.instance()
     bomb.set_name("bomb")
-    add_child(bomb)
-    var bombpos = Vector2()
-    var nmepos = get_position()
     bombpos.y = nmepos.y + 25
     bombpos.x = nmepos.x
+    add_child(bomb)
     get_node("bomb").set_position(bombpos)
 
 func _on_Velon_area_entered(area):
-	mainnode.connect("scoreinc", self, "score_increase")
 	alph -= .1
 	if alph > .1:
 		$AnimatedSprite.set("modulate",Color(1, 1, 1, alph))
