8e4d9bf2755ef56f88e97224eda51bd5c990a00e
Dan MacDonald <allcoms@gmail.com>
2018-03-15 20:06:05 +0000

    Add velon death animation

--- Velon.gd
+++ Velon.gd
@@ -2,6 +2,9 @@ extends Area2D
 
 export (PackedScene) var Bomb
 
+onready var death = get_node("Death")
+onready var sprite = get_node("Sprite")
+
 var alph = 1
 var timer = null
 var nmepos = get_position()
@@ -41,10 +44,16 @@ func bomb():
 func _on_Velon_area_entered(area):
 	alph -= .1
 	if alph > .1:
-		$AnimatedSprite.set("modulate",Color(1, 1, 1, alph))
+		$Sprite.set("modulate",Color(1, 1, 1, alph))
 	else:
 		get_node("/root/Main").score += 1
 		emit_signal("scoreup")
 		get_node("/root/Main/Explosion").play()
 		get_node("/root/Main").veloncount -= 1
-		queue_free()
+		death.interpolate_property(sprite, "rotation_degrees", 0, 720, 1.0, Tween.TRANS_QUAD, Tween.EASE_OUT)
+		death.interpolate_property(sprite, "scale", Vector2(0.33, 0.33), Vector2(0, 0), 1.0, Tween.TRANS_QUAD, Tween.EASE_OUT)
+		death.start()
+
+
+func _on_Death_tween_completed(object, key):
+	queue_free()
