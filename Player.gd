afaab705b388556f86614fbd0f4457fc87ef938f
Dan MacDonald <allcoms@gmail.com>
2018-03-15 19:32:22 +0000

    Add player death animation

--- Player.gd
+++ Player.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
+onready var death = get_node("Death")
+onready var sprite = get_node("Sprite")
+
+
 func _on_Player_area_entered(area):
 	var score = get_node("/root/Main").score
 	var highscore = get_node("/root/Main").highscore
@@ -7,6 +11,10 @@ func _on_Player_area_entered(area):
 		get_node("/root/Main").highscore = score
 	get_node("/root/Main").score = 0
 	get_node("/root/Main").update_score()
+	death.interpolate_property(sprite, "scale", Vector2(1.0, 1.0), Vector2(0, 0), 1.0, Tween.TRANS_BOUNCE, Tween.EASE_OUT)
+	death.start()
+
+func _on_Death_tween_completed(object, key):
 	set_name("DeadPlayer")
 	queue_free()
 	get_node("/root/Main").spawn_player()
