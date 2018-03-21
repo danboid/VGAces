a90ee3084a3974e9eead7e94ac9c885766037435
Dan MacDonald <allcoms@gmail.com>
2018-03-21 20:01:51 +0000

    Add Widehorn collision detection

--- Widehorn.gd
+++ Widehorn.gd
@@ -1,4 +1,10 @@
 extends Area2D
 
+var colr = 0.1
+
 func _on_Widehorn_area_entered(area):
-	pass
+	colr += .01
+	if colr < 1:
+		$Sprite.set("modulate",Color(colr, colr, colr, 1))
+	else:
+		queue_free()
