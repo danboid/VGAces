2e8bfa1717c9f5b5b37faeb2c395ddf24555808e
Dan MacDonald <allcoms@gmail.com>
2018-03-26 22:42:03 +0100

    Add Nukefall tween code to make sprite fade in and out on loop

---
+++ Nukefall.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+onready var fade = get_node("Fade")
+onready var sprite = get_node("Sprite")
+onready var faderange = [0, 1]
+
+func _ready():
+	_start_tween()
+
+func _start_tween():
+	fade.interpolate_property(sprite, "modulate", Color(1, 1, 1, faderange[0]), Color(1, 1, 1, faderange[1]), 1.0, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	fade.start()
+
+func _on_Fade_tween_completed(object, key):
+	faderange.invert()
+	_start_tween()
