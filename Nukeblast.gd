5884af1c05bb18c1159befbfba1fa39b2c1057e0
Dan MacDonald <allcoms@gmail.com>
2018-03-30 21:21:31 +0100

    Tweak Nukeblast size and duration

--- Nukeblast.gd
+++ Nukeblast.gd
@@ -6,12 +6,12 @@ onready var contract = get_node("Contract")
 var blastsize = 0
 
 func _ready():
-	blastsize = rand_range(1.0, 2.5)
-	blast.interpolate_property(self, "scale", Vector2(0, 0), Vector2(blastsize, blastsize), 2.0, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	blastsize = rand_range(1.0, 2.0)
+	blast.interpolate_property(self, "scale", Vector2(0, 0), Vector2(blastsize, blastsize), 1.0, Tween.TRANS_QUAD, Tween.EASE_OUT)
 	blast.start()
 	
 func _on_Blast_tween_completed(object, key):
-	contract.interpolate_property(self, "scale", Vector2(blastsize, blastsize), Vector2(0, 0), 2.0, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	contract.interpolate_property(self, "scale", Vector2(blastsize, blastsize), Vector2(0, 0), 1.0, Tween.TRANS_QUAD, Tween.EASE_OUT)
 	contract.start()
 
 func _on_Contract_tween_completed(object, key):
