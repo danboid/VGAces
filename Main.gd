3ad4f4e5ad9dbfb54f171e49d7ff134e3a6c89e5
Dan MacDonald <allcoms@gmail.com>
2018-03-30 22:14:08 +0100

    Add launch.ogg

--- Main.gd
+++ Main.gd
@@ -43,6 +43,7 @@ func _process(delta):
 	if Input.is_action_pressed("fire_nuke") and havenuke == true:
 		if ctrlpressed == false:
 			nukefire()
+			$Launch.play()
 			ctrlpressed = true
 	else:
 		ctrlpressed = false
